4617142d0cb90f720c7fb636ca3ac83e348248ad
Section1 <otinoco@digiocio.com>
2022-04-03 21:33:55 +0200

    CODE 5 - Web Server Basic

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict" //Per obtenir compatibilitat amb les ultimes versions
 
+const http = require('http');
 var mongoose= require("mongoose");
 mongoose.connect("mongodb://localhost:27017/curs_nodejs",(err, res) =>{
 if(err){
@@ -9,4 +10,17 @@ console.log("La base de dades está funcionant correctamente");
 }
 });
 
-console.log("Hola mòn");
+//console.log("Hola mòn");
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hola Mundo');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
+});
