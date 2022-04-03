5b5cf8c7ce13994ecb8ef8f224c05eb52973e3f6
Section1 <otinoco@digiocio.com>
2022-04-03 21:08:09 +0200

    CODE 4 - Db Connect MongoDb

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 "use strict" //Per obtenir compatibilitat amb les ultimes versions
 
+var mongoose= require("mongoose");
+mongoose.connect("mongodb://localhost:27017/curs_nodejs",(err, res) =>{
+if(err){
+throw err;
+}else{
+console.log("La base de dades está funcionant correctamente");
+}
+});
+
 console.log("Hola mòn");
