e203964d05cb5fb5f8ac0f6481dee05499d68bbf
Section1 <otinoco@digiocio.com>
2022-04-08 14:41:40 +0200

    CODE 8 - Exemple rebre dades

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ var app = express();
 
 //Carregar  rutes
 
+//Middlewares o tasques que s'han de realitzar abans de rebre la petició
 app.use(bodyParser.urlencoded({extended:false})); //Configurar  bodyparser
-app.use(bodyParser.json());
+app.use(bodyParser.json()); //Rebem el cos de la petició i el transformem en un json
 
 //configurar capçaleres
 
@@ -18,7 +19,8 @@ app.get("/get", (req, res) => {
 })
 
 app.post("/post", (req, res) => {
-    res.send("Soc un post")
+    //res.send("Soc un post")
+    res.json(req.body) //Retornem en forma de Json el que ens hagin enviat
 })
 
 app.put("/put", (req, res) => {
