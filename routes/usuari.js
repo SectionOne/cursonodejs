1c40735da13b348523e60c0681b81ef09238a1f8
Section1 <otinoco@digiocio.com>
2022-04-09 22:56:28 +0200

    CODE 17 - Exemple de ruta amb controlador assignat

--- routes/usuari.js
+++ routes/usuari.js
@@ -1,23 +1,10 @@
 'use strict'
 
 var express = require("express");
+var UsuariController = require("../controllers/usuari");
 
 var api = express.Router();
 
-api.get("/get", (req, res) => {
-    res.send("Soc un get")
-})
-
-api.post("/post", (req, res) => {
-    res.send("Soc un post")
-})
-
-api.put("/put", (req, res) => {
-    res.send("Soc un put")
-})
-
-api.delete("/delete", (req, res) => {
-    res.send("Soc un delete")
-})
+api.get("/tots", UsuariController.proves);
 // api.put("/actualitzar-usuari/:id?"); Afegim un ? al final, per poder dir que aquest parametre és opcional.
 module.exports = api;
