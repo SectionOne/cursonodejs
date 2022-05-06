d5cf2d34025ed5c7725c5960eb5e3e0e075291f0
Section1 <otinoco@digiocio.com>
2022-05-06 12:12:35 +0200

    CODE 24 - Controlador Update Img

--- controllers/usuari.js
+++ controllers/usuari.js
@@ -110,11 +110,26 @@ function borrarUsuari(req, res){
     });
 }
 
+function uploadImages(req, res){
+    var userId = req.params.id;
+    var file_name = "No pujat...";
+
+    //Anem a comprobar si rebem algo per les variables globals de la peticio "files"
+    if(req.files){
+        var file_path = req.files.image.path;
+        console.log(file_path); //Per provar
+    } else {
+        //Resposta per quan no rebem correctament l'arxiu
+        res.status(200).send({message: "No has pujat ninguna imatge..."});
+    }
+}
+
 module.exports = {
     proves,
     guardarUsuari,
     veureUsuari,
     veureTotsUsuari,
     actualitzarUsuari,
-    borrarUsuari
+    borrarUsuari,
+    uploadImages
 };
--- routes/usuari.js
+++ routes/usuari.js
@@ -4,6 +4,10 @@ var express = require("express");
 var UsuariController = require("../controllers/usuari");
 
 var api = express.Router();
+var multipart = require("connect-multiparty");
+
+//middleware
+var md_upload = multipart({ uploadDir: './uploads/users'});
 
 api.get("/tots", UsuariController.proves);
 api.post("/registre", UsuariController.guardarUsuari);
@@ -11,5 +15,6 @@ api.post("/veureusuari", UsuariController.veureUsuari);
 api.get("/veuretotsusuari", UsuariController.veureTotsUsuari);
 api.put("/actualitzar-usuari/:id", UsuariController.actualitzarUsuari);
 api.delete("/borrarusuari/:id", UsuariController.borrarUsuari);
+api.post("/upload-image-user/:id", [md_upload], UsuariController.uploadImages);
 // api.put("/actualitzar-usuari/:id?"); Afegim un ? al final, per poder dir que aquest parametre és opcional.
 module.exports = api;
