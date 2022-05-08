aaf14097d4cd292740e944c4838890d142f8e4cd
Section1 <otinoco@digiocio.com>
2022-05-08 13:25:20 +0200

    CODE 25 - Controlador Update Img Split

--- controllers/usuari.js
+++ controllers/usuari.js
@@ -117,7 +117,9 @@ function uploadImages(req, res){
     //Anem a comprobar si rebem algo per les variables globals de la peticio "files"
     if(req.files){
         var file_path = req.files.image.path;
-        console.log(file_path); //Per provar
+        var file_split = file_path.split("\\");
+        var file_name = file_split[2];
+        console.log(file_split); //Per provar
     } else {
         //Resposta per quan no rebem correctament l'arxiu
         res.status(200).send({message: "No has pujat ninguna imatge..."});
